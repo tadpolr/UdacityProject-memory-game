c2465459501d05d07d955d21acc129db76bc8e4f
Tadpol Rachatasumrit <tadpolr@trmbp.local>
2017-12-03 20:48:27 +0700

    Refractor function

--- js/app.js
+++ js/app.js
@@ -25,118 +25,139 @@ function shuffle(array) {
     return array;
 }
 
-function createStarRating() {
-    $(".stars").empty()
-    for (let i = 0; i < 3; i++) {
-        const starList = document.createElement("li")
-        const starIcon = document.createElement("i")
-        starIcon.className = "fa fa-star"
-
-        starList.appendChild(starIcon)
-
-        $(".stars").append(starList);
-    }
-
-}
-
+// Set card icon type
 const iconType = [ "diamond", "paper-plane-o", "anchor", "bolt", "cube", "leaf", "bicycle", "bomb"]
-const cardIcon = iconType.concat( iconType )
+const cardIcon = iconType.concat(iconType);
 
-const createDeck = function() {
-    
-    $(".deck").empty()
-    $(".moves").text(0)
-    createStarRating()
-
-    // Create new card deck
-    const shuffledCardIcon = shuffle( cardIcon );
+// Create new set of cards function
+function createDeck() {
+    const shuffledCardIcon = shuffle(cardIcon);
     for (let i = 0; i < shuffledCardIcon.length; i++){
-        const thisCardIcon = shuffledCardIcon[i]
+        const thisCardIcon = shuffledCardIcon[i];
         const iconClass = "fa fa-" + thisCardIcon.toString()
 
-        const icon = document.createElement("i")
-        icon.className = iconClass
+        const icon = document.createElement("i");
+        icon.className = iconClass;
 
-        const card = document.createElement("li")
-        card.className = "card"
+        const card = document.createElement("li");
+        card.className = "card";
 
-        card.appendChild(icon)
+        card.appendChild(icon);
         $(".deck").append(card);
     }
+}
 
-    // Set timer
-    let s = 0
-    let m = 0
-    function timer(){
-        s++
-        if (s === 60) {
-            m++
-            s = 0
-        }
-        let time = (m < 10 ? "0" + m.toString() : m.toString()) + ":" + (s < 10 ? "0" + s.toString() : s.toString())
-        $(".timer").text(time)
+// Count moves function
+let moves = 0
+let stars = 3
+function CountMove() {
+    moves++
+    $(".moves").text(moves);
+
+    // Change star rating on moves count
+    if (moves === 10) {
+        stars--;
+        $(".stars li i").slice(2).removeClass("fa-star").addClass("fa-star-o");
+    } else if (moves === 15) {
+        stars--;
+        $(".stars li i").slice(1,2).removeClass("fa-star").addClass("fa-star-o");
+    } else if (moves === 18) {
+        stars--;
+        $(".stars li i").slice(0,1).removeClass("fa-star").addClass("fa-star-o");
     }
-    let startTimer = setInterval(timer,1000)
+}
 
-    // Update moves and stars
-    let moveCounter = 0
-    function moveCounted() {
-        moveCounter++
-        $(".moves").text(moveCounter)
+// Create star rating function
+function createStars() {
+    $(".stars").empty();
+    for (let i = 0; i < 3; i++) {
+        const star = document.createElement("li");
+        const starIcon = document.createElement("i");
+        starIcon.className = "fa fa-star";
 
-        if (moveCounter === 10) {
-            $(".stars li i").slice(2).removeClass("fa-star").addClass("fa-star-o") 
-        } else if (moveCounter === 13) {
-            $(".stars li i").slice(1,2).removeClass("fa-star").addClass("fa-star-o") 
-        } else if (moveCounter === 15) {
-            $(".stars li i").slice(0,1).removeClass("fa-star").addClass("fa-star-o") 
+        star.appendChild(starIcon);
+
+        $(".stars").append(star);
+    }
+}
+
+// Create stopwatch function
+let s = 0
+let m = 0
+function createTimer() {
+    s++
+    if (s === 60) {
+        m++;
+        s = 0;
+    }
+    let time = (m < 10 ? "0" + m.toString() : m.toString()) + ":" + (s < 10 ? "0" + s.toString() : s.toString());
+    $(".timer").text(time);
+}
+
+let openedCard = [] // Use to store opened card when matching
+function clickCard() {
+    if (this.className === "card" && openedCard.length !== 2) {
+        const icon = this.children[0].className;
+        openedCard.push( icon );
+
+        $(this).addClass("open show matching");
+        
+        if (openedCard.length === 2) {
+            CountMove();
+            setTimeout (function(){     
+                if (openedCard[0] === openedCard [1]) {
+                    $(".matching").addClass("match");
+                } 
+                $(".matching").removeClass("open show matching");
+                openedCard.length = 0;
+                if ($(".match").length === cardIcon.length) {
+                    clearInterval(startTimer);
+                    congratulation();
+                }
+            }, 500);
         }
     }
+}
 
-    // Create congratulation modal
-    function congratulation() {
-        $("#CongratModal").css("display","block")
-        $("#Time").text($(".timer").text())
-        $("#TotalMove").text(moveCounter)
-        $("#PlayAgain").click(function() {
-            $("#CongratModal").css("display","none")
-            createDeck()
-        })
-        $("#StopPlaying").click(function() {
-            $("#CongratModal").css("display","none")
-        })
-    }
-
-    // Make cards clickable
-    let openedCard = []
-    $(".card").click(function() {
-        if (this.className === "card" && openedCard.length !== 2) {
-            const icon = this.children[0].className
-            openedCard.push( icon )
-
-            $(this).addClass("open show matching")
-            
-            if (openedCard.length === 2) {
-                moveCounted()
-                setTimeout (function(){     
-                    if (openedCard[0] === openedCard [1]) {
-                        $(".matching").addClass("match")
-                    } 
-                    $(".matching").removeClass("open show matching")
-                    openedCard.length = 0
-                    if ($(".match").length === cardIcon.length) {
-                        clearInterval(startTimer)
-                        congratulation()
-                    }
-                }, 500)
-            }
-        }
+// Create congratulation modal when win
+function congratulation() {
+    $("#CongratModal").css("display","block");
+    $("#Time").text($(".timer").text());
+    $("#TotalMovesCount").text(moves);
+    $("#TotalStarsCount").text(stars);
+    $("#PlayAgain").click(function() {
+        $("#CongratModal").css("display","none");
+        restart();
+    })
+    $("#StopPlaying").click(function() {
+        $("#CongratModal").css("display","none");
     })
 }
 
-$(".restart").click(createDeck)
 
-$(document).ready(createDeck())
+let startTimer // Use to store IntervalID
+function restart() {
+    $(".deck").empty();
+    $(".moves").text(0);
+    $(".timer").text("00:00");
+    moves = 0;
+    stars = 3;
+    s = 0;
+    m = 0;
+
+    createStars();
+    createDeck();
+    startTimer = setInterval(createTimer,1000);
+
+    $(".card").click(clickCard);
+}
+
+$(".restart").click(function() {
+    clearInterval(startTimer);
+    restart();
+})
+
+$(document).ready(restart());
 
 /*
  * set up the event listener for a card. If a card is clicked:
